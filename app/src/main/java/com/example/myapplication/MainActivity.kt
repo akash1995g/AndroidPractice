e3c285ce26df20aec792ac2b1de09454627972a8
Akash G <dragon.abhi8@gmail.com>
2023-03-03 21:46:18 +0530

    Viewmodel

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.example.myapplication
 
-import android.app.Activity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.myapplication.view.ui.main.MainFragment
 
-class MainActivity : Activity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
     }
 }
---
+++ app/src/main/java/com/example/myapplication/view/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.myapplication.view.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
